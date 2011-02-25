c926b99d2f97184ed1878c7b6e198b03ea44d1e6
Marc Nieper-Wisskirchen <marc@nieper-wisskirchen.de>
2011-02-25 17:39:44 +0100

    CSS fixes. Refactored Handlers.

--- freerealms.py
+++ freerealms.py
@@ -15,7 +15,8 @@ import version
 class CalculatedProperty(db.Property):
     data_type = None
     
-    def __init__(self, verbose_name=None, calc_func=(lambda model_instance: None), **kwds):
+    def __init__(self, verbose_name=None,
+                 calc_func=(lambda model_instance: None), **kwds):
         super(CalculatedProperty, self).__init__(verbose_name, **kwds)
         self.calc_func = calc_func
         
@@ -58,6 +59,125 @@ class ETag(object):
         
     def __str__(self):
         return self._hash.hexdigest()
+
+
+class PageRequestHandler(webapp.RequestHandler):
+
+    def __init__(self):
+        self.user = users.get_current_user()
+
+    def get(self):
+        self.page = self.page_factory()
+        if self.page.protected and not self.user:
+            self.redirect(users.create_login_url(self.request.uri))
+        user_id = self.user.user_id() if self.user else None
+        etag = ETag()
+        etag.update('User', user_id)
+        for key, value in self.page.generate_key_values():
+            etag.update(key, value)
+        etag_str = str(etag)
+        self.response.headers['ETag'] = '"%s"' % (etag_str,)
+        if etag_str in self.request.if_none_match:
+            self.error(304)
+            return
+        self.page.update_form_data(self.request)
+        self.render()
+        
+    def post(self):
+        self.page = self.page_factory()
+        if not hasattr(self.page, 'action'):
+            self.error(405)
+            return
+        self.page.update_form_data(self.request)
+        redirection = self.page.action(self.request, self.user)
+        if redirection:
+            self.error(303)
+            self.response.headers['Location'] = redirection
+            return
+        self.render()
+
+    def render(self):
+        url = (users.create_logout_url(self.request.uri) if self.user
+            else users.create_login_url(self.request.uri))
+        if self.user:
+            self.page.nickname = self.user.nickname()
+        self.page.url = url
+        self.page.update_page_data(self.user)
+        self.page.render(self.response)
+
+
+class Page(object):
+
+    protected = False
+
+    @classmethod
+    def requestHandler(cls):
+        return type("PageRequestHandler", (PageRequestHandler,), 
+            {'page_factory': cls})
+
+    def generate_key_values(self):
+        return ()
+        
+    def update_form_data(self, request):
+        pass
+
+    def update_page_data(self, user):
+        pass
+
+    def render(self, response):
+        path = os.path.join(os.path.dirname(__file__), self.template_file)
+        response.out.write(template.render(path, self.__dict__))
+
+
+class MainPage(Page):
+    template_file = 'index.html'
+
+    def update_form_data(self, request):
+        self.keywords = request.get('keywords')
+
+    def update_page_data(self, user):
+        q = Campaign.all()
+        for word in self.keywords.split():
+            q.filter('keywords =', word.lower())
+        q.order('-modified')
+        q.order('__key__')
+        self.campaigns = q.fetch(20)
+
+    def generate_key_values(self):
+        yield 'Counter', counter.get_count()
+
+
+class AddPage(Page):
+
+    template_file = 'add.html'
+    protected = True
+
+    def update_form_data(self, request):
+        self.name = request.get('name')
+        self.system = request.get('system', 'Unspecified')
+        self.description = request.get('description')
+
+    def action(self, request, user):
+        if not user:
+            self.error_msg = (
+                u"Not logged in. Please log in before you proceed.")
+            return
+
+        campaign = Campaign(key_name=self.name,
+                            description=self.description,
+                            system=self.system,
+                            gamemasters=[user])
+        # FIXME Bad key error if name is empty
+        def txn():
+            if Campaign.get_by_key_name(self.name):
+                return False
+            campaign.put()
+            return True
+        if db.run_in_transaction(txn):
+            counter.increment()
+            return request.relative_url('/') # FIXME
+        self.error_msg = (
+            u"There exists already a campaign by the name '%s'." % self.name)
 
 
 class FreeRealmsRequestHandler(webapp.RequestHandler):
@@ -144,14 +264,17 @@ class AddHandler(FreeRealmsRequestHandler):
     def update_form_data(self):
         self.data['name'] = self.request.get('name')
         self.data['system'] = self.request.get('system', 'Unspecified')
+        self.data['description'] = self.request.get('description')
 
     def action(self):
         if not self.user:
-            self.data['error_msg'] = u"Not logged in. Please log in before you proceed."
+            self.data['error_msg'] = (
+                u"Not logged in. Please log in before you proceed.")
             return
-            
+
         name = self.data['name']
         campaign = Campaign(key_name=name,
+                            description=self.data['description'],
                             system=self.data['system'],
                             gamemasters=[self.user])
         # FIXME Bad key error if name is empty
@@ -167,8 +290,8 @@ class AddHandler(FreeRealmsRequestHandler):
 
 
 application = webapp.WSGIApplication(
-                                     [('/', MainHandler),
-                                      ('/add.html', AddHandler),],
+                                     [('/', MainPage.requestHandler()),
+                                      ('/add.html', AddPage.requestHandler()),],
                                      debug=True)
 
 
